1c8ca19fc3ad22e61f9b64b11fa71b37b89a4020
Jafarov <ejafarov@GFCZ6D3.corp.netledger.com>
2021-08-13 00:42:14 +0200

    deleting unnecessary files

--- Examples/locationweb/src/main/java/com/eldar/location/repos/LocationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.eldar.location.repos;
-
-import com.eldar.location.entities.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationRepository extends JpaRepository<Location,Integer> {
-
-}
--- Examples/locationweb/src/main/java/com/eldar/location/utilities/EmailUtil.java
+++
@@ -1,5 +0,0 @@
-package com.eldar.location.utilities;
-
-public interface EmailUtil {
-    void sendEmail(String toAddress, String subject, String body);
-}
